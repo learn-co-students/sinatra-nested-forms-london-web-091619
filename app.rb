657b88f9680becc1e69934a5207f3e081bda57a7
github username <github email address>
2019-10-08 17:31:15 +0100

    Done.

--- app.rb
+++ app.rb
@@ -5,5 +5,29 @@ module FormsLab
 
     # code other routes/actions here
 
+    get "/" do 
+    
+      erb:root
+    end
+
+    post "/root"  do  #we need to write a form for the landing
+
+    erb :root
+
+
+    end
+
+    get '/new' do 
+
+    erb :'/pirates/new'
+
+    end
+
+    post '/pirates' do 
+      binding.pry
+      @pirates = Pirate.create(params)
+     
+      redirect :'/show'
+    end
   end
 end
